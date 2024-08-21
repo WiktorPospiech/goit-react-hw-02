707fb2ef5ac5eeb759e7d7d4c2d74a50c9a91fa3
WiktorPospiech <wiktor.pospiech@gmail.com>
2024-08-21 21:31:32 +0200

    step 2 added

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,19 +1,60 @@
-import opinions from "../Feedback/opinions.json";
+// import opinions from "../Feedback/opinions.json";
+import { useState } from "react";
+import styles from "./options.module.css";
 
-const OptionsList = ({ opinions }) => {
+const ClickCounter = () => {
+  const [values, setValues] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const updateG = () => {
+    setValues({
+      ...values,
+      good: values.good + 1,
+    });
+  };
+
+  const updateN = () => {
+    setValues({
+      ...values,
+      neutral: values.neutral + 1,
+    });
+  };
+
+  const updateB = () => {
+    setValues({
+      ...values,
+      bad: values.bad + 1,
+    });
+  };
   return (
-    <li>
-      {opinions.map((opinion) => {
-        return <button key={opinion.name}>{opinion.name}</button>;
-      })}
-    </li>
+    <div>
+      <button onClick={updateG}>Good</button>
+      <button onClick={updateN}>Neutral</button>
+      <button onClick={updateB}>Bad</button>
+      <ul className={styles.list}>
+        <li>Good: {values.good}</li>
+        <li>Neutral: {values.neutral}</li>
+        <li>Bad: {values.bad}</li>
+      </ul>
+    </div>
   );
 };
 
+// const OptionsList = () => {
+//   return (
+//     <>
+//       <ClickCounter />
+//     </>
+//   );
+// };
+
 export default function Options() {
   return (
-    <ul>
-      <OptionsList opinions={opinions}></OptionsList>
-    </ul>
+    <ClickCounter />
+    // <ul>
+    //   <OptionsList opinions={opinions}></OptionsList>
+    // </ul>
   );
 }
